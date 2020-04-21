299f3f060039ebfb2e3e1e80c65075fca0df78ed
Andreas Asatera <ryusuke005@gmail.com>
2020-04-21 12:46:48 +0700

    commit

--- zoomeye.sh
+++ zoomeye.sh
@@ -12,24 +12,28 @@ banner
 
 printf "Masukkan url universitas?: "
 read universitas;
+ip=$(dig +short $universitas | awk '{ print ; exit }')
+# json=$(curl -# "https://api.shodan.io/shodan/host/{${ip}}?key={OZi7Lq6PPz8B15jO1nFa1Hagt5NulEBI}")
+# result=$(echo $json | jq ".data | .[]")
+# host=$(echo $result | jq ".http.host" | sed -e 's/"//g' | awk {'print "Proxy: " $1,$2'})
+# port=$(echo $result | jq ".port" | sed -e 's/"//g' | awk {'print "Port: " $1,$2'})
+# status=$(echo $result | jq ".data" | sed -e 's/"//g' | grep -Eo "HTTP.*" | cut -d "\\" -f1 | awk {'print "Status: " $1,$2'})
+# echo -e "\n$host"
+# echo -e "\n$port"
+# echo -e "\n$status"=
+
 echo -e "\n(login ke server)"
 json=$(curl -# -X POST https://api.zoomeye.org/user/login -d '{ "username": "ryusuke005@gmail.com", "password": "1q0cBUV1VN7w" }')
 access_token=$(echo $json | jq ".access_token" | sed -e 's/"//g')
 
 echo -e "\n(mencari proxy)"
-if [ -n "$1" ]; then
-	query=$(curl -# -X GET "https://api.zoomeye.org/host/search?query=hostname:$universitas&page=$1" -H "Authorization: JWT $access_token")
-else
-	query=$(curl -# -X GET "https://api.zoomeye.org/host/search?query=hostname:$universitas&page=1" -H "Authorization: JWT $access_token")
-fi
+query=$(curl -# -X GET "https://api.zoomeye.org/host/search?query=ip:$ip" -H "Authorization: JWT $access_token")
 result=$(echo $query | jq ".matches | .[]")
-# host=$(echo $result | jq ".ip" | sed -e 's/"//g' | awk {'print "Proxy: " $1,$2'})
-# port=$(echo $result | jq ".portinfo | .port" | sed -e 's/"//g' | awk {'print "Port: " $1,$2'})
-# app=$(echo $result | jq ".portinfo | .app" | sed -e 's/"//g' | awk {'print "App: " $1,$2'})
-# banner=$(echo $result | jq ".portinfo | .banner" | sed -e 's/"//g' | grep -Eo "HTTP.*" | cut -d "\\" -f1 | awk {'print "Response: " $1,$2'})
-# echo -e "\n$host"
-# echo -e "\n$port"
-# echo -e "\n$app"
-# echo -e "\n$banner"
-lists=$(echo $result | jq '"\(.ip):\(.portinfo | .port)	| App: \(.portinfo | .app), \(.portinfo | .banner) "'| sed -e 's/"//g' | awk {'print "[*]" $1,$2,$3,$4,$5,$6'})
-echo -e "\n$lists"
+host=$(echo $result | jq ".ip" | sed -e 's/"//g' | awk {'print "Proxy: " $1,$2'})
+port=$(echo $result | jq ".portinfo | .port" | sed -e 's/"//g' | awk {'print "Port: " $1,$2'})
+app=$(echo $result | jq ".portinfo | .app" | sed -e 's/"//g' | awk {'print "App: " $1,$2'})
+banner=$(echo $result | jq ".portinfo | .banner" | sed -e 's/"//g' | grep -Eo "HTTP.*" | cut -d "\\" -f1 | awk {'print "Response: " $1,$2'})
+echo -e "\n$host"
+echo -e "\n$port"
+echo -e "\n$app"
+echo -e "\n$banner"
